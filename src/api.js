337acc593af16210551e17ed007fbfc5636e70c4
Josh Butcher <sjwb1999@gmail.com>
2026-03-30 16:01:07 -0700

    update api.js, index.js, and index.html. This will also update styles.css.

--- src/api.js
+++ src/api.js
@@ -2,18 +2,17 @@ export default class CurrencyAPI {
   static async getCurrencyInfo() {
     try {
       const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_Key}/latest/USD`);
-      const jsonResponse = await response.json();
-      if (!response.ok) {
-        const newError = `${jsonResponse.result} /// ${jsonResponse["error-type"]}`
+      const resultJson = await response.json();
+      if (resultJson.result === "error") {
+        console.log(resultJson);
+        const newError = `${resultJson.result} /// ${resultJson["error-type"]}`
         throw new Error(newError);
+      } else if (!response.ok) {
+        throw new Error(`ERROR: ${resultJson.message}`)
       }
-
-      return jsonResponse;
+      return resultJson;
     } catch(error) {
-      return {
-        success: false,
-        error: error.message
-      }
+      return error;
     }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import CurrencyAPI from './api';
+import { convertValues } from './calculation';
+
+document.addEventListener("DOMContentLoaded", async function() {
+  const request = await CurrencyAPI.getCurrencyInfo();
+  if (request.result === `success`) {
+    createList(request);
+  } 
+});
+
+function createList(source) {
+  const list = source["conversion_rates"];
+  const newList = Object.entries(list);
+  
+  newList.forEach((key, value) => {
+
+  });
+}
 
